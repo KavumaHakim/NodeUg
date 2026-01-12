202b7b8fd7cfda202db0a2886ce9264393c6fabf
Kavuma Hakim <hakimkavuma99@gmail.com>
2026-01-12 17:18:47 +0300

    fix: update login form layout and styling for improved user experience

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,41 +6,28 @@ import { Link } from 'react-router-dom'
 
 const Login = () =>{
   return (
-    <div className='flex flex-col items-center justify-center h-screen gap-4 bg-linear-to-br from-[#FB8200] to-[#8B00DB]'>
-      <Form className='relative flex flex-col gap-4
-            bg-white/30 backdrop-blur-xl
-            p-10 rounded-[60px]
-            border border-white/40
-            shadow-xl
-            max-w-md w-full m-5' method='post' action='/login'>
-
+    <div className='flex flex-col items-center justify-center h-screen gap-0 bg-linear-to-br from-[#FB8200] to-[#8B00DB]'>
+      <img src="../../public/logo.svg" alt="Logo" className='w-50 h-50'/>
+      <Form className='relative  flex flex-col gap-2 bg-white/20 backdrop-blur-xl p-10 rounded-[60px] border border-white/40 shadow-xl max-w-md w-full m- mt-0' method='post' action='/#'>
+        
         <label htmlFor="email" className='text-[#8B00DB] ml-1'>Email</label>
-        <input type="email" id="email" className='w-full px-4 py-3
-         bg-white/40 backdrop-blur-md
-         border border-white/40
-         rounded-xl
-         text-slate-800 placeholder-slate-500
-         shadow-sm
-         outline-none
-         focus:border-white/70
-         focus:ring-2 focus:ring-white/50' />
+        <input type="email" id="email" className='w-full px-4 py-3 bg-white/40 backdrop-blur-md border border-white/40 rounded-full text-slate-800 placeholder-slate-500 shadow-sm outline-none focus:border-white/70 focus:ring-2 focus:ring-white/50' />
 
         <label htmlFor="password" className='text-[#8B00DB] ml-1'>Password</label>
-        <input type="password" id="password" className='w-full px-4 py-3
-         bg-white/40 backdrop-blur-md
-         border border-white/40
-         rounded-xl
-         text-slate-800 placeholder-slate-500
-         shadow-sm
-         outline-none
-         focus:border-white/70
-         focus:ring-2 focus:ring-white/50' />
+        <input type="password" id="password" className='w-full px-4 py-3 bg-white/40 backdrop-blur-md border border-white/40 rounded-full text-slate-800 placeholder-slate-500 shadow-sm outline-none focus:border-white/70 focus:ring-2 focus:ring-white/50' />
 
-        <div className='flex justify-center gap-5 mt-4'>
-          <Button type="submit" className='w-1/2'>Login</Button>
-          <Link to={"#"} className='w-1/2 text-center'>Sign Up</Link>
+        <Link to={"/#"} className='text-white text-center'>Forgot Password</Link>
+
+        <div className='flex justify-center gap-5 mt-5'>
+          <Button type="submit" className='w-1/2 rounded-full bg-white text-[#A4188B] hover:bg-white/80 hover:cursor-pointer'>Login</Button>
+          <Link to={"/#"} className='flex items-center justify-center w-1/2 rounded-full bg-white text-[#A4188B] text-center'>Sign Up</Link>
+
+          
         </div>
-
+        <div className='flex justify-evenly'>
+          <Link to={"/#"} className='text-white text-center'>Terms of Service</Link>
+          <Link to={"/#"} className='text-white text-center'>Privacy Policy</Link>
+        </div>
       </Form>
     </div>
   )
