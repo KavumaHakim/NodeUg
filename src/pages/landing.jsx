59d5faf4763d04ea510e693608122048557ae769
Kavuma Hakim <hakimkavuma99@gmail.com>
2026-01-12 17:31:33 +0300

    fix: update landing page header text

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button"
 function Landing() {
     return (
         <main className='w-screen h-screen flex flex-col justify-center items-center bg-black'>
-            <h1 className="text-6xl font-bold text-white wrap-break-word">Coming Soon!</h1>
+            <h1 className="text-6xl font-bold text-white wrap-break-word"> Landing Page Coming Soon!</h1>
         </main>
 )
 }
