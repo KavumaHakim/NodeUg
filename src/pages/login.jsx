bd3f8b17ff3c09ea65b54dd2a74ca1ab177da0e9
Kavuma Hakim <hakimkavuma99@gmail.com>
2026-01-12 18:14:07 +0300

    fix: update logo import path and add new logo SVG file
    - Changed the import path of the logo in login.jsx from '../../public/logo.svg' to '../logo.svg'.
    - Added a new logo.svg file with SVG content to the src directory.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,7 @@ import '../index.css'
 import { Button } from "@/components/ui/button"
 import { Form } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import logo from '../../public/logo.svg'
+import logo from '../logo.svg'
 
 const Login = () =>{
   return (
